8a8db5c2698834a240893a63413fd07663bdc396
L41se <manh.kien.21122012@gmail.com>
2023-07-21 20:42:19 +0700

    origin

--- MD_1/B-8_MD1_Biến/chuyen_doi_tien_te.js
+++ MD_1/B-8_MD1_Biến/chuyen_doi_tien_te.js
@@ -1,4 +1,15 @@
 function changeMoney(){
-    let inputAmount = document.getElementById("amount").value
-    alert(inputAmount)
+    let inputAmount = document.getElementById("amount").value;
+    let From = document.getElementById("From").value;
+    let To = document.getElementById("To").value;
+    let Result = "";
+     if (From == "USD" && To == "VND") {
+         Result = "Result: " + (inputAmount *23000) + "D"
+     } else if (From == "VND" && To == "USD") {
+         Result = "Result: " + (inputAmount /23000) + "$"
+         else if (From == "VND" && To == "VND") {
+             Result = "Result: " + inputAmount + "D"
+             else Result = "Result: " + inputAmount + "$"
+         }
+     }
 }
